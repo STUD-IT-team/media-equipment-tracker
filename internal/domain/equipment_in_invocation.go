a4829b57fecf8f2b46a170606e60a920980cc17d
Katerina Permyakova <c.k4terin4@mail.ru>
2026-03-29 11:50:28 +0300

    Разделила equipment_in_invocation на 2 структуры

--- internal/domain/equipment_in_invocation.go
+++ internal/domain/equipment_in_invocation.go
@@ -7,11 +7,24 @@ type EquipmentInInvocation struct {
 	EquipmentID  uuid.UUID                   `gorm:"type:uuid;primaryKey"`
 	Status       EquipmentInInvocationStatus `gorm:"type:equipment_in_invocation_status"`
 
-	Invocation *EquipmentInvocation `gorm:"foreignKey:InvocationID;constraint:OnDelete:CASCADE"`
-	Equipment  *Equipment           `gorm:"foreignKey:EquipmentID;constraint:OnDelete:CASCADE"`
+	//Invocation *EquipmentInvocation `gorm:"foreignKey:InvocationID;constraint:OnDelete:CASCADE"`
+	Equipment *Equipment `gorm:"foreignKey:EquipmentID;constraint:OnDelete:CASCADE"`
 }
 
 func (EquipmentInInvocation) TableName() string {
+	return "equipment_in_invocation"
+}
+
+type InvocationsForEquipment struct {
+	InvocationID uuid.UUID                   `gorm:"type:uuid;primaryKey"`
+	EquipmentID  uuid.UUID                   `gorm:"type:uuid;primaryKey"`
+	Status       EquipmentInInvocationStatus `gorm:"type:equipment_in_invocation_status"`
+
+	Invocation *EquipmentInvocation `gorm:"foreignKey:InvocationID;constraint:OnDelete:CASCADE"`
+	//Equipment  *Equipment           `gorm:"foreignKey:EquipmentID;constraint:OnDelete:CASCADE"`
+}
+
+func (InvocationsForEquipment) TableName() string {
 	return "equipment_in_invocation"
 }
 
