9467e47b5afcb8cf0acc128a095891ce7f4428ca
impervguin <leon53000@yandex.ru>
2026-04-16 22:19:02 +0300

    Написал тесты для репозитория организаций и исправил сохранение связей

---
+++ internal/adapters/postgres/pgorganization/helpers_test.go
@@ -0,0 +1,64 @@
+package pgorganization_test
+
+import (
+	"media-equipment-tracker/internal/domain"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func newOrg() *domain.Organization {
+	return &domain.Organization{
+		ID:   uuid.New(),
+		Name: "org",
+	}
+}
+
+func newUser() *domain.User {
+	return &domain.User{
+		ID:           uuid.New(),
+		FullName:     "test",
+		Email:        uuid.NewString() + "@mail.ru",
+		HashPassword: "hash",
+		Nice:         domain.DefaultNice,
+	}
+}
+
+func newDept() *domain.Department {
+	return &domain.Department{
+		ID:   uuid.New(),
+		Name: "dept",
+	}
+}
+
+func newInvocation(depID *uuid.UUID, userID uuid.UUID) *domain.EquipmentInvocation {
+	return &domain.EquipmentInvocation{
+		ID:                  uuid.New(),
+		EventName:           "event",
+		StartTime:           time.Now().Add(-time.Hour * 96),
+		EndTime:             time.Now().Add(time.Hour * 96),
+		EquipmentReturnTime: nil,
+		SdCardReturnTime:    nil,
+		Status:              domain.InvocationCreated,
+		DepartmentID:        depID,
+		UserID:              userID,
+	}
+}
+
+func newStudioInvocation(orgID *uuid.UUID, depID *uuid.UUID, userID *uuid.UUID, adminID *uuid.UUID) *domain.StudioInvocation {
+	now := time.Now()
+
+	return &domain.StudioInvocation{
+		ID:                  uuid.New(),
+		EventName:           "Event",
+		ShootingDescription: "Desc",
+		StartTime:           now,
+		EndTime:             now.Add(time.Hour),
+		Status:              domain.StudioCreated,
+
+		OrganizationID: orgID,
+		DepartmentID:   depID,
+		AdminID:        adminID,
+		UserID:         *userID,
+	}
+}
