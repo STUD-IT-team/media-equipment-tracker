b4ec72443e7a8f9354a2c0b53389d0990307069a
impervguin <leon53000@yandex.ru>
2026-04-16 20:56:02 +0300

    Дополнил тесты на репозиторий пользователя, дописал ручное обновление связей в update и create

--- internal/adapters/postgres/pguser/repository.go
+++ internal/adapters/postgres/pguser/repository.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type PostgresUserRepository struct {
@@ -102,19 +103,67 @@ func (r *PostgresUserRepository) Reload(user *domain.User, opts ...domain.UserOp
 }
 
 func (r *PostgresUserRepository) Create(user *domain.User) error {
-	err := r.db.Create(user).Error
-	if err != nil {
+	tx := r.db.Begin()
+	defer tx.Rollback()
+	if err := tx.Omit(clause.Associations).Create(user).Error; err != nil {
 		return errs.NewRepositoryError("create", err)
 	}
-	return nil
+
+	if user.Organizations != nil {
+		tx.Exec("DELETE FROM user_organization WHERE user_id = ?", user.ID)
+		for _, o := range user.Organizations {
+			if err := tx.Exec(
+				"INSERT INTO user_organization (user_id, organization_id) VALUES (?, ?)",
+				user.ID, o.ID,
+			).Error; err != nil {
+				return err
+			}
+		}
+	}
+	if user.Departments != nil {
+		tx.Exec("DELETE FROM user_department WHERE user_id = ?", user.ID)
+		for _, d := range user.Departments {
+			if err := tx.Exec(
+				"INSERT INTO user_department (user_id, department_id, role) VALUES (?, ?, ?)",
+				user.ID, d.DepartmentID, d.Role,
+			).Error; err != nil {
+				return err
+			}
+		}
+	}
+	return tx.Commit().Error
 }
 
 func (r *PostgresUserRepository) Update(user *domain.User) error {
-	err := r.db.Save(user).Error
-	if err != nil {
+	tx := r.db.Begin()
+	defer tx.Rollback()
+	if err := tx.Omit(clause.Associations).Updates(user).Error; err != nil {
 		return errs.NewRepositoryError("update", err)
 	}
-	return nil
+
+	if user.Organizations != nil {
+		tx.Exec("DELETE FROM user_organization WHERE user_id = ?", user.ID)
+		for _, o := range user.Organizations {
+			if err := tx.Exec(
+				"INSERT INTO user_organization (user_id, organization_id) VALUES (?, ?)",
+				user.ID, o.ID,
+			).Error; err != nil {
+				return err
+			}
+		}
+	}
+	if user.Departments != nil {
+		tx.Exec("DELETE FROM user_department WHERE user_id = ?", user.ID)
+		for _, d := range user.Departments {
+			if err := tx.Exec(
+				"INSERT INTO user_department (user_id, department_id, role) VALUES (?, ?, ?)",
+				user.ID, d.DepartmentID, d.Role,
+			).Error; err != nil {
+				return err
+			}
+		}
+	}
+	return tx.Commit().Error
 }
 
 func (r *PostgresUserRepository) Delete(id uuid.UUID) error {
